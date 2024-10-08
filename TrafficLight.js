7f427faa77448cb6d61e97cdd8132a71183a5732
Youna <165991705+YounaV1@users.noreply.github.com>
2024-10-08 10:28:54 +0330

    Add files via upload

---
+++ TrafficLight.js
@@ -0,0 +1,15 @@
+function allgreen()
+{
+        let imageg = document.getElementById('green')
+        let imagey = document.getElementById('yellow')
+        let imager = document.getElementById('red')
+        if()
+}
+function allyellow()
+{
+    
+}
+function allred()
+{
+    
+}
