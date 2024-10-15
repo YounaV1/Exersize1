47ba1eeec104c3edd21d17696a1d87f39edf9554
Youna <165991705+YounaV1@users.noreply.github.com>
2024-10-15 10:30:32 +0330

    Add files via upload

---
+++ Zard.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const rl = readline.createInterface
+({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.question("Enter a number", function(input)
+{
+    const n = parseInt(input)
+    let i="W"
+    
+
+
+    i+="w!"
+    rl.close()
+});
